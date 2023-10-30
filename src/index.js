32cc738bafacd89d413659c31ba991e0fd188077
Jan Groß <git@minzkraut.com>
2023-10-30 11:22:05 +0100

    Spotify API integration

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
+require('dotenv').config();
 const express = require('express')
+const { SpotifyApi } = require("@spotify/web-api-ts-sdk");
 
 const app = express()
 const port = 3000
 
+const spotifyApi = SpotifyApi.withClientCredentials(
+    process.env.SPOTIFY_CLIENT_ID,
+    process.env.SPOTIFY_CLIENT_SECRET
+);
+
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
-app.get('/', (req, res) => {
-    let purl = req.query.playlist;
-    res.render('index', { purl: purl });
-})
+function urlToID (url) {
+    //https://open.spotify.com/playlist/2GBjRoOMS1Zhb0owyfKIzs?si=1d8dc4db4a494dfd
+    let id = url.split('/').pop().split("?")[0];
+    return id;
+}
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-})
+app.get('/', async (req, res) => {
+    let purl = urlToID(req.query.playlist);
+    let spotifyRes = await spotifyApi.playlists.getPlaylist(purl,market=undefined, fields= "name,tracks(total,items(track(album(name))))" );
+
+    res.render('index', { purl: purl, spotifyRes: spotifyRes });
+});
+
+(async () => {
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`)
+    })
+})();
