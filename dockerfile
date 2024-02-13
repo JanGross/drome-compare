df05cd12e1f9af991ce954649875acc3bfbe35b4
Minz <git@minzkraut.com>
2024-02-14 00:13:13 +0100

    Change docker files for new DB creation

--- dockerfile
+++ dockerfile
@@ -3,6 +3,5 @@ ENV NODE_ENV production
 WORKDIR /app
 COPY --chown=node:node . .
 RUN npm ci --only=production
-USER node
 EXPOSE 8000
 CMD ["node", "src/index.js"]
--- src/initDB.js
+++ src/initDB.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 // Connect to SQLite database (or create if it doesn't exist)
 const db = new sqlite3.Database('./db/compare.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
     if (err) {
-        console.error('Error opening database', err.message);
+        console.error('Error creating database', err.message);
     } else {
         console.log('Connected to the SQLite database.');
     }
